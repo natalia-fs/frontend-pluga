ca721d02fac98c8f8bfa40cc470908c350048466
Natalia <natalia-321s@outlook.com>
2022-07-20 23:31:12 -0300

    feat: searchbar working with keyboard control

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, KeyboardEvent } from 'react';
 import ReactPaginate from 'react-paginate';
 import { AppModal } from './components/AppModal';
 import Card from './components/Card';
@@ -89,15 +89,40 @@ function App() {
     setFilteredData(data);
   }
 
+  // Making searchbar works with keyboard control
+  function handleActivateSearchByKeyboard(event: KeyboardEvent){
+    if(event.key === 'Enter'){
+      handleSearchApp();
+    }
+  }
+
+  function handleClearSearchByKeyboard(event: KeyboardEvent){
+    if(event.key === 'Enter'){
+      setFilteredData(data);
+    }
+  }
+
   return (
     <div className='App'>
 
       <div className="searchbar__container">
-        <span role="button" onClick={handleSearchApp} title="Buscar">
+        <span
+          role="button"
+          title="Buscar"
+          tabIndex={0}
+          onClick={handleSearchApp}
+          onKeyDown={handleActivateSearchByKeyboard}
+        >
           <img src="./icons/search.svg" alt="Search icon" />
         </span>
         <input type="text" ref={inputSearchbar} placeholder='Buscar ferramenta' />
-        <span role="button" onClick={handleClearSearch} title="Limpar busca">
+        <span
+          role="button"
+          title="Limpar busca"
+          tabIndex={0}
+          onClick={handleClearSearch}
+          onKeyDown={handleClearSearchByKeyboard}
+        >
           <img src="./icons/close.svg" alt="Search icon" />
         </span>
       </div>
